fe163be5d39d2410f5e7796b8df6042c6d7bfd85
aionick <nick@nuco.io>
2019-02-07 09:12:35 -0500

    Created a builder for avm call transactions

--- modAionImpl/test/org/aion/zero/impl/vm/resources/AvmCallTransactionBuilder.java
+++ modAionImpl/test/org/aion/zero/impl/vm/resources/AvmCallTransactionBuilder.java
@@ -1,8 +1,7 @@
 package org.aion.zero.impl.vm.resources;
 
 import java.math.BigInteger;
-import org.aion.avm.core.dappreading.JarBuilder;
-import org.aion.avm.core.util.CodeAndArguments;
+import org.aion.avm.api.ABIEncoder;
 import org.aion.base.type.AionAddress;
 import org.aion.base.vm.VirtualMachineSpecs;
 import org.aion.crypto.ECKey;
@@ -10,8 +9,8 @@ import org.aion.mcf.vm.Constants;
 import org.aion.zero.types.AionTransaction;
 
 /**
- * A builder class that makes constructing a signed avm transaction that will deploy a new dApp
- * simple.
+ * A builder class that makes constructing a signed avm transaction that will call into an existing
+ * dApp simple.
  *
  * If the same method is invoked multiple times with different arguments each time before the
  * transaction is built, then the final invocation takes precedence and defines that field.
@@ -22,15 +21,14 @@ import org.aion.zero.types.AionTransaction;
  * Alternatively, the {@code resetBuilder()} method can be used to reset all fields as if the
  * builder had been newly created.
  */
-public final class AvmCreateTransactionBuilder {
+public class AvmCallTransactionBuilder {
     private static final String ERROR_MESSAGE = "Cannot build avm create transaction with ";
     private ECKey senderKey = null;
     private BigInteger nonce = null;
-    private Class<?> mainClass = null;
-    private Class<?>[] dependencies = new Class<?>[0];
+    private String method = null;
+    private Object[] parameters = new Object[0];
     private BigInteger value = BigInteger.ZERO;
-    private byte[] clinitArgs = new byte[0];
-    private long energyLimit = Constants.NRG_CREATE_CONTRACT_MAX;
+    private long energyLimit = Constants.NRG_TRANSACTION_MAX;
     private long energyPrice = 1;
 
     /**
@@ -42,7 +40,7 @@ public final class AvmCreateTransactionBuilder {
      * @param senderKey The key of the sender.
      * @return this builder.
      */
-    public AvmCreateTransactionBuilder senderKey(ECKey senderKey) {
+    public AvmCallTransactionBuilder senderKey(ECKey senderKey) {
         this.senderKey = senderKey;
         return this;
     }
@@ -55,21 +53,32 @@ public final class AvmCreateTransactionBuilder {
      * @param nonce The nonce of the sender.
      * @return this builder.
      */
-    public AvmCreateTransactionBuilder senderNonce(BigInteger nonce) {
+    public AvmCallTransactionBuilder senderNonce(BigInteger nonce) {
         this.nonce = nonce;
         return this;
     }
 
     /**
-     * Sets the class that has the main entry point method in the dapp to the specified class.
+     * Sets the name of the method in the existing dApp to be invoked by this call.
      *
      * <b>This field must be set.</b>
      *
-     * @param main The main class in the dapp.
+     * @param methodName The method to invoke.
      * @return this builder.
      */
-    public AvmCreateTransactionBuilder dappMainClass(Class<?> main) {
-        this.mainClass = main;
+    public AvmCallTransactionBuilder methodToInvoke(String methodName) {
+        this.method = methodName;
+        return this;
+    }
+
+    /**
+     * Sets the parameters that will be used to invoke the specified dApp method with.
+     *
+     * @param parameters The invocation parameters.
+     * @return this builder.
+     */
+    public AvmCallTransactionBuilder parametersToInvokeMethodWith(Object... parameters) {
+        this.parameters = parameters;
         return this;
     }
 
@@ -79,38 +88,8 @@ public final class AvmCreateTransactionBuilder {
      * @param value The value to transfer.
      * @return this builder.
      */
-    public AvmCreateTransactionBuilder valueToTransferToContract(BigInteger value) {
+    public AvmCallTransactionBuilder valueToTransferToContract(BigInteger value) {
         this.value = value;
-        return this;
-    }
-
-    /**
-     * Sets any classes that the main class depends on to the specified classes.
-     *
-     * @param dependencies The class dependencies.
-     * @return this builder.
-     */
-    public AvmCreateTransactionBuilder dappDependencyClasses(Class<?>... dependencies) {
-        this.dependencies = dependencies;
-        return this;
-    }
-
-    /**
-     * Sets the dapp's clinit arguments to the specified arguments.
-     *
-     * The clinit arguments are the special arguments that get passed into the dapp during its
-     * original construction phase, this is where all static fields and blocks get initialized.
-     *
-     * These arguments can be captured using the blockchain runtime's getData() method.
-     *
-     * The avm strips the clinit after the first launch of the dapp, so these arguments can only
-     * be provided once.
-     *
-     * @param arguments The clinit arguments.
-     * @return this builder.
-     */
-    public AvmCreateTransactionBuilder dappClinitArguments(byte[] arguments) {
-        this.clinitArgs = arguments;
         return this;
     }
 
@@ -120,7 +99,7 @@ public final class AvmCreateTransactionBuilder {
      * @param limit The transaction energy limit.
      * @return this builder.
      */
-    public AvmCreateTransactionBuilder energyLimit(long limit) {
+    public AvmCallTransactionBuilder energyLimit(long limit) {
         this.energyLimit = limit;
         return this;
     }
@@ -131,27 +110,26 @@ public final class AvmCreateTransactionBuilder {
      * @param price The transaction energy price.
      * @return this builder.
      */
-    public AvmCreateTransactionBuilder energyPrice(long price) {
+    public AvmCallTransactionBuilder energyPrice(long price) {
         this.energyPrice = price;
         return this;
     }
 
     /**
-     * Constructs a new transaction for creating a new avm dApp.
+     * Constructs a new transaction for calling into an existing avm dApp.
      *
      * The following fields must have been set before calling this method or it will fail:
      *   - sender key
      *   - sender nonce
-     *   - dApp main class
+     *   - dApp method to invoke
      *
      * The remaining optional fields have the following default values if they have not been set:
      *   - value is {@link BigInteger#ZERO}
-     *   - dependencies is an empty array
-     *   - clinit arguments is an empty array
-     *   - energy limit is {@link Constants#NRG_CREATE_CONTRACT_MAX}
+     *   - dApp method parameters is an empty array
+     *   - energy limit is {@link Constants#NRG_TRANSACTION_MAX}
      *   - energy price is {@code 1}
      *
-     * @return a new transaction for creating a new avm dApp.
+     * @return a new transaction for calling into an existing avm dApp.
      */
     public AionTransaction buildAvmCreateTransaction() {
         if (this.senderKey == null) {
@@ -160,8 +138,8 @@ public final class AvmCreateTransactionBuilder {
         if (this.nonce == null) {
             throw new IllegalStateException(ERROR_MESSAGE + "no sender nonce.");
         }
-        if (this.mainClass == null) {
-            throw new IllegalStateException(ERROR_MESSAGE + "no main dapp class.");
+        if (this.method == null) {
+            throw new IllegalStateException(ERROR_MESSAGE + "no dapp method to call.");
         }
 
         AionTransaction transaction = new AionTransaction(
@@ -169,7 +147,7 @@ public final class AvmCreateTransactionBuilder {
             AionAddress.wrap(this.senderKey.getAddress()),
             null,
             this.value.toByteArray(),
-            getJarFileBytes(),
+            getEncodingOfMethodCall(),
             this.energyLimit,
             this.energyPrice,
             VirtualMachineSpecs.AVM_CREATE_CODE);
@@ -185,17 +163,15 @@ public final class AvmCreateTransactionBuilder {
     public void resetBuilder() {
         this.senderKey = null;
         this.nonce = null;
-        this.mainClass = null;
+        this.method = null;
         this.value = BigInteger.ZERO;
-        this.dependencies = new Class<?>[0];
-        this.clinitArgs = new byte[0];
-        this.energyLimit = Constants.NRG_CREATE_CONTRACT_MAX;
+        this.parameters = new Object[0];
+        this.energyLimit = Constants.NRG_TRANSACTION_MAX;
         this.energyPrice = 1;
     }
 
-    private byte[] getJarFileBytes() {
-        byte[] jar = JarBuilder.buildJarForMainAndClasses(this.mainClass, this.dependencies);
-        return new CodeAndArguments(jar, this.clinitArgs).encodeToBytes();
+    private byte[] getEncodingOfMethodCall() {
+        return ABIEncoder.encodeMethodArguments(this.method, this.parameters);
     }
 
 }
