702765872510cb683f9b17564246a8003d14411c
Alexandra <alexandra@nuco.io>
2019-02-11 14:35:17 -0500

    fast sync manager check for complete world state

--- modAionImpl/src/org/aion/zero/impl/sync/FastSyncManager.java
+++ modAionImpl/src/org/aion/zero/impl/sync/FastSyncManager.java
@@ -1,10 +1,16 @@
 package org.aion.zero.impl.sync;
 
+import com.google.common.annotations.VisibleForTesting;
 import java.util.Arrays;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.LinkedBlockingQueue;
 import org.aion.base.util.ByteArrayWrapper;
+import org.aion.zero.impl.AionBlockchainImpl;
+import org.aion.zero.impl.types.AionBlock;
 
 /**
  * Directs behavior for fast sync functionality.
@@ -16,9 +22,20 @@ public final class FastSyncManager {
     private boolean enabled;
 
     private final Map<ByteArrayWrapper, byte[]> importedTrieNodes = new ConcurrentHashMap<>();
+    private final BlockingQueue<ByteArrayWrapper> requiredStateNodes = new LinkedBlockingQueue<>();
+
+    private AionBlock pivot = null;
+    private long pivotNumber = -1;
+    private final AionBlockchainImpl chain;
 
     public FastSyncManager() {
         this.enabled = false;
+        this.chain = null;
+    }
+
+    public FastSyncManager(AionBlockchainImpl chain) {
+        this.enabled = true;
+        this.chain = chain;
     }
 
     public void addImportedNode(ByteArrayWrapper key, byte[] value) {
@@ -31,6 +48,14 @@ public final class FastSyncManager {
         return enabled
                 && importedTrieNodes.containsKey(key)
                 && Arrays.equals(importedTrieNodes.get(key), value);
+    }
+
+    @VisibleForTesting
+    void setPivot(AionBlock pivot) {
+        Objects.requireNonNull(pivot);
+
+        this.pivot = pivot;
+        this.pivotNumber = pivot.getNumber();
     }
 
     /** Changes the pivot in case of import failure. */
@@ -52,6 +77,29 @@ public final class FastSyncManager {
     public boolean isComplete() {
         // TODO: implement check for completeness
         return false;
+    }
+
+    @VisibleForTesting
+    boolean isCompleteWorldState() {
+        if (pivot == null) {
+            return false;
+        } else {
+            // get root of pivot
+            byte[] root = pivot.getStateRoot();
+
+            // traverse trie from root to find missing nodes
+            Set<ByteArrayWrapper> missing = chain.traverseTrieFromNode(root, DatabaseType.STATE);
+
+            // clearing the queue to ensure we're not still requesting already received nodes
+            requiredStateNodes.clear();
+
+            if (missing.isEmpty()) {
+                return true;
+            } else {
+                requiredStateNodes.addAll(missing);
+                return false;
+            }
+        }
     }
 
     public void updateRequests(ByteArrayWrapper topmostKey, Set<ByteArrayWrapper> referencedKeys) {
